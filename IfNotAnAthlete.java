42a298798c88ebd1497fd31d12e9ee74980b1d2f
yjkim <lime3737@gmail.com>
2025-11-06 16:04:22 +0900

    feat: IfNotAnAthlete.java 초기 파일 추가

---
+++ IfNotAnAthlete.java
@@ -0,0 +1,20 @@
+public class IfNotAnAthlete {
+    
+    private Boolean isAthlete = false; //운동선수 여부
+    private Boolean isDancer = false; //무용수 여부
+    private Boolean isAmerican = false; //미국인 여부
+    private Boolean isGerman = false; //독일인 여부
+    private Boolean isKrExperienced = false; //한국 생활 경험 여부
+    private Boolean wearsWhiteJacket = false; // 흰색 재킷 착용 여부
+    private Boolean knowsKimchi = false; // 김치 먹을 줄 아는 지 여부
+
+    public static void main(String[] args) {
+
+        IfNotAnAthlete A = new IfNotAnAthlete(); // 
+
+        if(!A.isAthlete) {
+    
+        }
+        
+    }
+}
