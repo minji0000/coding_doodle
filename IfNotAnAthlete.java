2b4dad967a5a0c3490a64d9546ce593dabaf1943
yjkim <lime3737@gmail.com>
2025-11-07 12:33:17 +0900

    보기 대입해서 가정하기

--- IfNotAnAthlete.java
+++ IfNotAnAthlete.java
@@ -1,49 +1,38 @@
 public class IfNotAnAthlete {
     
-    private Boolean isAthlete = false; //운동선수 여부
-    private Boolean isDancer = false; //무용수 여부
-    private Boolean isAmerican = false; //미국인 여부
-    private Boolean isGerman = false; //독일인 여부
-    private Boolean isKrExperienced = false; //한국 생활 경험 여부
-    private Boolean wearsWhiteJacket = false; // 흰색 재킷 착용 여부
-    private Boolean knowsKimchi = false; // 김치 먹을 줄 아는 지 여부
+    private static Boolean isAthlete; //운동선수 여부
+    private static Boolean isDancer; //무용수 여부
+    private static Boolean isAmerican; //미국인 여부
+    private static Boolean isGerman; //독일인 여부
+    private static Boolean isKrExperienced; //한국 생활 경험 여부
+    private static Boolean wearsWhiteJacket; // 흰색 재킷 착용 여부
+    private static Boolean knowsKimchi; // 김치 먹을 줄 아는 지 여부
 
     public static void main(String[] args) {
 
-        IfNotAnAthlete A = new IfNotAnAthlete(); // 
+        for(int i=0; i<5; i++) {
 
-        //A가 운동선수가 아니라면 무용수이다.
-        if(!A.isAthlete) {
-            A.isDancer = true;
-        
-        }else {
-            A.isDancer = false;
+            //① "독일인 무용수이다"
+            if(i==1) {
+                isGerman = true;
+                isDancer = true;
+                isAthlete = false;
+                isAmerican = false;
+                isKrExperienced = false;
+                wearsWhiteJacket= false;
+                knowsKimchi = false;
+            
+            }else if(i==2) {
+
+            }else if(i==3) {
+                
+            }else if(i==4) {
+                
+            }else if(i==5) {
+                
+            }
+
         }
 
-        //모든 무용수들은 흰색 재킷을 입는다.
-        if(A.isDancer) {
-            A.wearsWhiteJacket = true;
-        }
-
-        //그러나 A는 초록색 재킷을 입는다. 
-        A.wearsWhiteJacket = false;
-
-        //만약 A가 운동선수라면
-        if(A.isAthlete) {
-            //그는 미국인이거나 독일인이다. 
-            A.isAmerican = true;
-            A.isGerman = true;
-        }
-
-        if(A.isGerman && !A.isKrExperienced) {
-            A.knowsKimchi = false;
-        }
-
-
-
-
-
-
-        
     }
 }
