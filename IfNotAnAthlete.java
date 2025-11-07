575fad1658d01264182291be2bdf8c5b5b9059be
yjkim <lime3737@gmail.com>
2025-11-07 10:37:29 +0900

    if문 나열

--- IfNotAnAthlete.java
+++ IfNotAnAthlete.java
@@ -12,9 +12,38 @@ public class IfNotAnAthlete {
 
         IfNotAnAthlete A = new IfNotAnAthlete(); // 
 
+        //A가 운동선수가 아니라면 무용수이다.
         if(!A.isAthlete) {
-    
+            A.isDancer = true;
+        
+        }else {
+            A.isDancer = false;
         }
+
+        //모든 무용수들은 흰색 재킷을 입는다.
+        if(A.isDancer) {
+            A.wearsWhiteJacket = true;
+        }
+
+        //그러나 A는 초록색 재킷을 입는다. 
+        A.wearsWhiteJacket = false;
+
+        //만약 A가 운동선수라면
+        if(A.isAthlete) {
+            //그는 미국인이거나 독일인이다. 
+            A.isAmerican = true;
+            A.isGerman = true;
+        }
+
+        if(A.isGerman && !A.isKrExperienced) {
+            A.knowsKimchi = false;
+        }
+
+
+
+
+
+
         
     }
 }
