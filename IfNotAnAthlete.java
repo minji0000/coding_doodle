aabbcac20830cd6fd0620506ed0c1cad7fc31200
yjkim <lime3737@gmail.com>
2025-11-12 17:00:05 +0900

    보기 분기처리

--- IfNotAnAthlete.java
+++ IfNotAnAthlete.java
@@ -1,35 +1,56 @@
 public class IfNotAnAthlete {
     
-    private static Boolean isAthlete; //운동선수 여부
-    private static Boolean isDancer; //무용수 여부
-    private static Boolean isAmerican; //미국인 여부
-    private static Boolean isGerman; //독일인 여부
-    private static Boolean isKrExperienced; //한국 생활 경험 여부
-    private static Boolean wearsWhiteJacket; // 흰색 재킷 착용 여부
-    private static Boolean knowsKimchi; // 김치 먹을 줄 아는 지 여부
-
     public static void main(String[] args) {
 
-        for(int i=0; i<5; i++) {
+        for(int i=1; i<=5; i++) {
 
-            //① "독일인 무용수이다"
+            boolean isAthlete = false;
+            boolean isDancer = false;
+            boolean isAmerican = false; 
+            boolean isGerman = false;
+            boolean isKrExperienced = false;
+            boolean wearsWhiteJacket = false; //그러나 A는 초록색 자켓을 입는다.
+            boolean knowsKimchi = true; // P6: A는 김치를 먹을 줄 안다 (확정 사실)
+
+            //1. 독일인 무용수이다
             if(i==1) {
                 isGerman = true;
                 isDancer = true;
                 isAthlete = false;
                 isAmerican = false;
                 isKrExperienced = false;
-                wearsWhiteJacket= false;
-                knowsKimchi = false;
             
+            //2. 미국인 운동선수이다.
             }else if(i==2) {
+                isGerman = false;
+                isDancer = false;
+                isAthlete = true;
+                isAmerican = true;
+                isKrExperienced = false;
 
+            //3. 무용수이면서 운동선수이다.
             }else if(i==3) {
+                isGerman = false;
+                isDancer = true;
+                isAthlete = true;
+                isAmerican = false;
+                isKrExperienced = false;
                 
+            //4. 한국 생활 경험이 없는 독일인이다.
             }else if(i==4) {
+                isGerman = true;
+                isDancer = false;
+                isAthlete = false;
+                isAmerican = false;
+                isKrExperienced = false;
                 
+            //5. 한국 생활 경험이 있는 미국인이다.
             }else if(i==5) {
-                
+                isGerman = false;
+                isDancer = false;
+                isAthlete = false;
+                isAmerican = true;
+                isKrExperienced = true;
             }
 
         }
